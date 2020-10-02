3df141ecbbf6a4d6b98c43e0bc9c50c615a04599
Martin <martin.cervantes.9718@gmail.com>
2020-10-02 15:19:07 -0500

    MONGO environment variable

--- server/config/config.js
+++ server/config/config.js
@@ -18,7 +18,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/coffee';
 } else {
-    urlDB = 'mongodb+srv://strider:bd0gkPRontj0LT6I@cluster0.w4vga.mongodb.net/coffee?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
